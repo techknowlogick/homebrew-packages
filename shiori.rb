93a9bbfd70efd67b7ab4d4cf9aae964c9f89daeb
Matti Ranta <matti@mdranta.net>
2019-03-22 20:41:45 -0400

    add shiori formula

---
+++ shiori.rb
@@ -0,0 +1,35 @@
+require "formula"
+
+class Shiori < Formula
+  homepage "https://github.com/techknowlogick/shiori"
+  head "https://github.com/techknowlogick/shiori.git"
+
+  stable do
+    version "0.7.0"
+    url "https://dl.techknowlogick.com/shiori/#{version}/shiori-darwin-10.6-amd64"
+    sha256 `curl -s https://dl.techknowlogick.com/shiori/#{version}/shiori-darwin-10.6-amd64.sha256`.split(" ").first
+  end
+
+  devel do
+    version "nightly"
+    url "https://dl.techknowlogick.com/shiori/#{version}/shiori-darwin-10.6-amd64"
+    sha256 `curl -s https://dl.techknowlogick.com/shiori/#{version}/shiori-darwin-10.6-amd64.sha256`.split(" ").first
+  end
+
+  head do
+    url "#{head}", :branch => "master"
+    depends_on "go" => :build
+  end
+
+  def install
+    case
+    when build.head?
+      ENV["GO111MODULE"]="on"
+      system "make dep && make build"
+      bin.install "#{buildpath}/shiori" => "shiori"
+    else
+      bin.install "#{buildpath}/shiori-darwin-10.6-amd64" => "shiori"
+    end
+  end
+end
+
