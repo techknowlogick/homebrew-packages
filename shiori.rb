355d57043f767d8f595c1de1f209fd59c43c2aff
techknowlogick <hello@techknowlogick.com>
2019-03-22 21:55:15 -0400

    Update shiori.rb

--- shiori.rb
+++ shiori.rb
@@ -16,22 +16,8 @@ class Shiori < Formula
     sha256 `curl -s https://dl.techknowlogick.com/shiori/#{version}/shiori-darwin-10.6-amd64.sha256`.split(" ").first
   end
 
-  head do
-    url "https://github.com/techknowlogick/shiori.git", :branch => "master"
-    depends_on "go" => :build
-    depends_on "node" => :build
-  end
-
   def install
-    case
-    when build.head?
-      ENV["GO111MODULE"]="on"
-      system "make dep"
-      system "make build"
-      bin.install "#{buildpath}/shiori" => "shiori"
-    else
-      bin.install "#{buildpath}/shiori-darwin-10.6-amd64" => "shiori"
-    end
+    bin.install "#{buildpath}/shiori-darwin-10.6-amd64" => "shiori"
   end
 end
 
